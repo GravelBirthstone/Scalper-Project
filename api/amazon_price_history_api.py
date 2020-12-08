5de64b16c53b04f4f476c22251e04972e6d5a21a
Alex Peña <attn.alex.p@gmail.com>
2020-12-08 12:25:41 -0800

    PS4 Price trend chart is working just needs tweaking

--- api/amazon_price_history_api.py
+++ api/amazon_price_history_api.py
@@ -2,7 +2,7 @@ import requests
 
 url = "https://amazon-price-history.p.rapidapi.com/api/us/price_history"
 
-querystring = {"asin":"B01LOP8EZC", "price_type":"amazon"}
+querystring = {"asin":"B071CV8CG2"}
 
 headers = {
     'x-rapidapi-key': "38e058ddb8msh0ab4bb9902ac5b2p1d7aa3jsn10ae3807ccee",
@@ -15,3 +15,6 @@ print(response.text)
 
 
 # https://rapidapi.com/Megatvini/api/amazon-price-history/endpoints
+
+
+
